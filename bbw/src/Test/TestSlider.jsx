5a5c5d7257c92163f2c4df0e384dcacbb672d1ef
Arpit Surana <suranaarpit34@gmail.com>
2022-05-03 18:20:43 +0530

    .txt updated, made a test folder for testing

---
+++ bbw/src/Test/TestSlider.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from 'react'
+import { TodaysTopOfferSlider } from '../API/TodaysTopOfferSlider'
+
+const TestSlider = () => {
+  return (
+    <div>TestSlider</div>
+  )
+}
+
+export default TestSlider
