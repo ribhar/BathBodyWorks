00da792dade7ede6509199ce15ef8603d1dfe09a
MeAnkur <uic17bca1427@gmail.com>
2022-05-09 17:21:15 +0530

    final - Ankur

---
+++ bbw/src/Contributors/Ankur/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Aroma from './Aroma/Aroma';
+import Gift from './Gift/Gift';
+import Hand from './Hand/Hand';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Aroma/> */}
+      <Gift/>
+      {/* <Hand/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ bbw/src/Contributors/Ankur/src/Gift/Gift.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import styles from "./gift.module.css";
+import { gift_set } from "./gift_set";
+const Gift = () => {
+  const [soap, setSoap] = useState(gift_set);
+
+  // Sorting : --
+  //  useEffect(() => {
+  const selectChang = (e) => {
+    console.log(e.target.value);
+    if (e.target.value === "lth") {
+      gift_set.sort((a, b) => {
+        return a.Amount - b.Amount;
+      }); // (a.Amount>b.Amount ? -1 :1)
+      setSoap(gift_set);
+    }
+  };
+  //  },[]);
+
+  return (
+    <>
+      {/* Top of the page */}
+      <div className={styles.anTop}>
+        <h1>Gift Sets</h1>
+      </div>
+
+      {/* Sorting and Filteration */}
+      <div className={styles.anSort}>
+        SORT BY
+        <select className={styles.anSortBox} onChange={selectChang}>
+          <option value="">Best Matches</option>
+          <option value="lth">Price Low to High</option>
+          <option value="htl">Price High to Low</option>
+          <option value="pop">Most Popular</option>
+          <option value="top">Top Seller</option>
+        </select>
+        {/* onClick={handelClick} */}
+      </div>
+
+      {/* Cards */}
+      <div className={styles.anMainCard}>
+        {gift_set.map((item) => {
+          return (
+            <div className={styles.anCard} key={item.id}>
+            <center>
+              <img style={{ height: "200px" }} src={item.pImg} alt="Image" />
+              <b  className={styles.anBold}>{item.category}</b><br/>
+              <b>{item.name}</b>
+              <h4 className={styles.anCat1}>{item.category1}</h4><br/>
+              <h2 className={styles.anAmount}>${item.Amount}</h2>
+              <button>ADD TO BAG</button>
+              <h2>{item.Rating}</h2>
+              
+            </center>
+          </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Gift;
