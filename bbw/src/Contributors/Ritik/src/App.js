68f83ccfaa63d0c3de0ac748963235c8a0c59610
Ritik Bhardwaj <varribhar@gmail.com>
2022-05-09 17:08:03 +0530

    commit

---
+++ bbw/src/Contributors/Ritik/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import SignUPSignIn from './pages/SignUPSignIn';
+
+
+function App() {
+  return (
+    <div className="App">
+      <SignUPSignIn/>
+    </div>
+  );
+}
+
+export default App;
